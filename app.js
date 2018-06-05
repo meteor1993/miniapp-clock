64baf02259c8b835a8affa98961662dc75c335cb
weishiyao <inwsy@hotmail.com>
2018-06-06 00:11:36 +0800

    add login

--- app.js
+++ app.js
@@ -9,15 +9,33 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log(res);
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         wx.request({
-          url: 'https://mobile.kaixindaka.com',
+          url: 'https://mobile.kaixindaka.com/dailyclock/miniapp/user/login',
+          method: 'GET',
+          dataType: 'json',
           data: {
             code: res.code
+          },
+          success: res => {
+            console.log(res);
           }
         })
       }
     })
+
+    // wx.openSetting({
+    //   success: (res) => {
+    //     if (res.authSetting["scope.userInfo"]) {
+    //       // 授权成功
+    //     } else {
+    //       //仍然没有授权
+    //     }
+    //   }
+    // })
+
+
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -25,13 +43,16 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log(res);
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
+              console.log(this.globalData);
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
+                console.log(res);
               }
             }
           })
